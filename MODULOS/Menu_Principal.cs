a83a628ac5f9b9122d99b4d90c44fcccf3663806
rama <rama@DESKTOP-43N09TV>
2025-05-06 20:55:44 -0300

    .

---
+++ MODULOS/Menu_Principal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Practico1.MODULOS
+{
+    internal class Menu_Principal
+    {
+        public void Verificar_Partida()
+        {
+            Console.WriteLine($"Quedan los siguientes jugadores {jugadores}");
+        }
+    }
+}
