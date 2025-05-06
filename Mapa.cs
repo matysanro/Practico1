c290d7237a331b68fb318fa5b47d3dea99720a7c
Thiago Lozano <thiagolozano47@gmail.com>
2025-05-06 20:57:35 -0300

    .

---
+++ Mapa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Mapa
+{
+    static void Main()
+    {
+        string[] mapas = { "Fortaleza Sombría", "Sector 9", "Horizonte Perdido" };
+
+        Console.WriteLine("Lista de mapas:");
+        foreach (string mapa in mapas)
+        {
+            Console.WriteLine(mapa);
+        }
+    }
+}
